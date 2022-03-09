353bf230d411367bb1778e009b3e240dc47cf944
william-wigemo-nti-johanneberg <william.wigemo@elev.ga.ntig.se>
2022-03-09 10:19:38 +0100

    feat: seperate model file

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require "sqlite3"
 require "slim"
 require "bcrypt"
 require "sinatra/reloader"
+require "./model.rb"
 
 enable :sessions
 
@@ -42,76 +43,20 @@ end
 # START HELPERS
 
 helpers do
-  def groups
-    db = connect_db()
-    user_id = session[:user_id]
-
-    if (!user_id)
-      # return empty if not logged in
-      return []
-    end
-
-    # get groups where user is a member of
-    groups = db.execute("SELECT 
-                        *
-                      FROM
-                        chat_groups
-                      INNER JOIN groups_users
-                      ON (chat_groups.id = groups_users.group_id)
-                      WHERE groups_users.user_id = ?
-                      ", user_id)
-
-    return groups
+  def groups(user_id = params[:user_id])
+    return get_groups_of_user(user_id)
   end
 
   def members(group_id = params[:group_id])
-    if (!group_id)
-      return []
-    end
-
-    db = connect_db()
-
-    members = db.execute("SELECT
-                          users.id,
-                          users.username,
-                          users_group_roles.group_role_id
-                        FROM
-                          users
-                        INNER JOIN groups_users
-                          ON groups_users.user_id = users.id
-                        LEFT JOIN users_group_roles
-                          ON users_group_roles.user_id = users.id
-                          AND users_group_roles.group_role_id = (SELECT id FROM group_roles WHERE group_roles.group_id = ?)
-                        WHERE groups_users.group_id = ?
-                        ", group_id, group_id)
-
-    return members
+    return get_members_in_group(group_id)
   end
 
   def role(role_id = params[:role_id])
-    if (!role_id)
-      return nil
-    end
-
-    db = connect_db()
-
-    role = db.execute("SELECT * FROM group_roles WHERE id = ?", role_id).first
-    return role
+    return get_role(role_id)
   end
 end
 
 # END HELPERS
-
-# START FUNCTIONS
-
-def connect_db(path = "db/database.db")
-  db = SQLite3::Database.new(path)
-  db.results_as_hash = true
-
-  return db
-end
-
-# END FUNCTIONS
 
 # START ROUTES
 
@@ -129,25 +74,12 @@ post("/login") do
   username = params[:username]
   password = params[:password]
 
-  db = connect_db()
-
-  # get user with username
-  user = db.execute("SELECT * FROM users WHERE username = ?", username).first
-
-  if user
-    # compare password
-    pwd_digest = user["pwd_digest"]
-
-    if BCrypt::Password.new(pwd_digest) == password
-      # login user
-      session[:user_id] = user["id"]
-      redirect("/")
-    else
-      return "Invalid password or username"
-    end
+  begin
+    login_user(username, password)
+  rescue => exception
+    return exception.message
   else
-    # show error message: invalid username
-    return "Invalid username"
+    redirect("/")
   end
 end
 
@@ -160,51 +92,20 @@ post("/users/new") do
   password = params[:password]
   confirm_password = params[:confirm_password]
 
-  # validate password
-  if !(password == confirm_password)
-    # show error message: passwords not matching
-    return "Passwords not matching"
+  begin
+    register_user(username, password, confirm_password)
+  rescue => exception
+    return exception.message
+  else
+    redirect("/")
   end
-
-  db = connect_db()
-
-  # check for existing users
-  user = db.execute("SELECT * FROM users WHERE username = ?", username).first
-  if user
-    # show error message: user exists already
-    return "Username is already in use"
-  end
-
-  # one-way encrypt password
-  pwd_digest = BCrypt::Password.create(password)
-
-  # create new user
-  db.execute("INSERT INTO users (username, pwd_digest) VALUES (?, ?)", username, pwd_digest)
-
-  # get id of new user
-  user_id = db.execute("SELECT id FROM users WHERE username = ?", username)
-
-  # save user_id in session
-  session[:user_id] = user_id
-
-  redirect("/")
 end
 
 post("/groups") do
   group_name = params[:name]
   user_id = session[:user_id]
 
-  db = connect_db()
-
-  # create group
-  db.execute("INSERT INTO chat_groups (name, creator) VALUES (?, ?)", group_name, user_id)
-  group_id = db.last_insert_row_id
-
-  puts "GROUP:"
-  puts group_id
-
-  # add users to the group
-  db.execute("INSERT INTO groups_users (user_id, group_id) VALUES (?, ?)", user_id, group_id)
+  create_group(user_id, group_name)
 
   redirect("/")
 end
@@ -217,20 +118,8 @@ get("/groups/{group_id}") do
   group_id = params[:group_id]
   user_id = session[:user_id]
 
-  db = connect_db()
-
-  # get messages from group with sender username
-  messages = db.execute("SELECT 
-                          messages.id,
-                          users.username,
-                          messages.message
-                        FROM messages
-                        LEFT JOIN users
-                        ON messages.user_id = users.id
-                        WHERE group_id = ?
-                        ", group_id)
-
-  group = db.execute("SELECT * FROM chat_groups WHERE id = ?", group_id).first
+  group = get_group_by_id(group_id)
+  messages = get_messages_in_group(group_id)
 
   slim(:"groups/show", locals: { group: group, messages: messages })
 end
@@ -240,10 +129,7 @@ post("/messages") do
   message = params[:message]
   user_id = session[:user_id]
 
-  db = connect_db()
-
-  # create message
-  db.execute("INSERT INTO messages (message, group_id, user_id) VALUES (?, ?, ?)", message, group_id, user_id)
+  create_message_in_group(group_id, user_id, message)
 
   redirect("/groups/#{group_id}")
 end
@@ -252,9 +138,7 @@ get("/groups/{group_id}/edit") do
   group_id = params[:group_id]
   role_id = params[:role_id]
 
-  db = connect_db()
-
-  group_roles = db.execute("SELECT * FROM group_roles WHERE group_id = ?", group_id)
+  group_roles = get_roles_in_group(group_id)
 
   slim(:"groups/edit", locals: { group_roles: group_roles })
 end
@@ -263,44 +147,15 @@ post("/groups/{group_id}/update") do
   new_member_username = params[:new_member_username]
   group_id = params[:group_id]
 
-  puts "EDITING GROUP #{group_id}"
-
-  db = connect_db()
-
-  # get the user id of user with username
-  user = db.execute("SELECT id FROM users WHERE username = ?", new_member_username).first
-
-  if (!user)
-    # display error message
-    return "No user found with username #{new_member_username}"
-  end
-
-  user_id = user["id"]
-  puts "Checking if users #{user_id} exists in group #{group_id}"
-
-  # check if user is already a member of the group
-  is_member = db.execute("SELECT
-              1
-              FROM groups_users
-              WHERE user_id = ?
-              AND group_id = ?", user_id, group_id).first
-
-  if (is_member)
-    # display error message
-    return "User is already a member of the group"
-  end
-
-  puts "Adding user #{user_id} to group #{group_id}"
-  db.execute("INSERT INTO groups_users (user_id, group_id) VALUES (?, ?)", user_id, group_id)
+  add_member_to_group(group_id, new_member_username)
 
   redirect("/groups/#{group_id}")
 end
 
 get("/groups/:group_id/roles") do
-  db = connect_db()
   group_id = params[:group_id]
 
-  roles = db.execute("SELECT * FROM group_roles WHERE group_id = ?", group_id)
+  roles = get_roles_in_group(group_id)
 
   slim(:"roles/index", locals: { roles: roles })
 end
@@ -316,9 +171,7 @@ post("/groups/:group_id/roles") do
   can_delete = params[:can_delete]
   can_kick = params[:can_kick]
 
-  db = connect_db()
-
-  db.execute("INSERT INTO group_roles (group_id, title, canDelete, canKick) VALUES (?, ?, ?, ?)", group_id, title, can_delete, can_kick)
+  create_role(group_id, title, can_delete, can_kick)
 
   redirect("/groups/#{group_id}/roles")
 end
@@ -332,7 +185,6 @@ get("/groups/:group_id/roles/:role_id/edit") do
 end
 
 post("/groups/:group_id/roles/:role_id/update") do
-  # TODO: update group role
   group_id = params[:group_id]
   role_id = params[:role_id]
 
@@ -340,19 +192,19 @@ post("/groups/:group_id/roles/:role_id/update") do
   can_delete = params[:can_delete]
   can_kick = params[:can_kick]
 
-  db = connect_db()
-
-  db.execute("UPDATE group_roles
-              SET title = ?,
-                  canDelete = ?,
-                  canKick = ?
-              WHERE id = ?", title, can_delete, can_kick, role_id)
+  update_role(role_id, title, can_delete, can_kick)
 
   redirect("/groups/#{group_id}/roles/#{role_id}/edit")
 end
 
-get("/groups/:group_id/roles/:role_id/destroy") do
+post("/groups/:group_id/roles/:role_id/destroy") do
   # TODO: delete role and remove role from all users with the role
+  role_id = params[:role_id]
+  group_id = params[:group_id]
+
+  delete_role_in_group(group_id, role_id)
+
+  redirect("/groups/#{group_id}/roles")
 end
 
 post("/groups/:group_id/members/:user_id/update") do
@@ -360,40 +212,7 @@ post("/groups/:group_id/members/:user_id/update") do
   group_id = params[:group_id]
   user_id = params[:user_id]
 
-  db = connect_db()
-
-  current_role_id = db.execute("SELECT 
-                                users_group_roles.group_role_id
-                              FROM 
-                                users_group_roles
-                              LEFT JOIN users
-                                ON users.id = users_group_roles.user_id
-                              INNER JOIN group_roles
-                                ON group_roles.id = users_group_roles.group_role_id
-                              WHERE group_roles.group_id = ?
-                              AND users.id = ?
-                              ", group_id, user_id).first
-
-  if (current_role_id)
-    if !role_id
-      # update role_id
-      db.execute("UPDATE users_group_roles
-                SET group_role_id = ?
-                WHERE users_group_roles.group_role_id =
-                  (SELECT id 
-                    FROM group_roles 
-                  WHERE group_id = ?)
-                AND user_id = ?", role_id, group_id, user_id)
-    else
-      # delete role from user
-      db.execute("DELETE FROM users_group_roles
-                  WHERE users_group_roles.group_role_id = (SELECT id FROM group_roles WHERE group_id = ?)
-                  AND user_id = ?", group_id, user_id)
-    end
-  else
-    # create new relation
-    db.execute("INSERT INTO users_group_roles (group_role_id, user_id) VALUES (?, ?)", role_id, user_id)
-  end
+  update_role_of_user_in_group(group_id, user_id, role_id)
 
   redirect("/groups/#{group_id}/edit")
 end
---
+++ model.rb
@@ -0,0 +1,281 @@
+def connect_db(path = "db/database.db")
+  db = SQLite3::Database.new(path)
+  db.results_as_hash = true
+
+  return db
+end
+
+def login_user(username, password)
+  db = connect_db()
+
+  # get user with username
+  user = db.execute("SELECT * FROM users WHERE username = ?", username).first
+
+  if user
+    # compare password
+    pwd_digest = user["pwd_digest"]
+
+    if BCrypt::Password.new(pwd_digest) == password
+      # login user
+      session[:user_id] = user["id"]
+    else
+      raise "Invalid password or username"
+    end
+  else
+    raise "Invalid username"
+  end
+end
+
+def register_user(username, password, confirm_password)
+  # validate password
+  if !(password == confirm_password)
+    # show error message: passwords not matching
+    raise "Passwords not matching"
+  end
+
+  db = connect_db()
+
+  # check for existing users
+  user = db.execute("SELECT * FROM users WHERE username = ?", username).first
+  if user
+    # show error message: user exists already
+    raise "Username is already in use"
+  end
+
+  # one-way encrypt password
+  pwd_digest = BCrypt::Password.create(password)
+
+  # create new user
+  db.execute("INSERT INTO users (username, pwd_digest) VALUES (?, ?)", username, pwd_digest)
+
+  # get id of new user
+  user_id = db.execute("SELECT id FROM users WHERE username = ?", username)
+
+  # save user_id in session
+  session[:user_id] = user_id
+end
+
+def create_group(user_id, group_name)
+  db = connect_db()
+
+  # create group
+  db.execute("INSERT INTO chat_groups (name, creator) VALUES (?, ?)", group_name, user_id)
+  group_id = db.last_insert_row_id
+
+  # add users to the group
+  db.execute("INSERT INTO groups_users (user_id, group_id) VALUES (?, ?)", user_id, group_id)
+end
+
+def get_group_by_id(group_id)
+  # TODO
+  group = db.execute("SELECT * FROM chat_groups WHERE id = ?", group_id).first
+  return group
+end
+
+def update_group(group_id)
+  # TODO
+end
+
+def delete_group(group_id)
+  # TODO
+end
+
+def create_message_in_group(group_id, user_id, message)
+  db = connect_db()
+
+  # create message
+  db.execute("INSERT INTO messages (message, group_id, user_id) VALUES (?, ?, ?)", message, group_id, user_id)
+end
+
+def get_messages_in_group(group_id)
+  db = connect_db()
+
+  # get messages from group with sender username
+  messages = db.execute("SELECT 
+                          messages.id,
+                          users.username,
+                          messages.message
+                        FROM messages
+                        LEFT JOIN users
+                        ON messages.user_id = users.id
+                        WHERE group_id = ?
+                        ", group_id)
+  return messages
+end
+
+def update_message_in_group(group_id)
+  # TODO
+end
+
+def delete_message_in_group(message_id)
+  # TODO
+end
+
+def get_groups_of_user(user_id)
+  db = connect_db()
+  user_id = session[:user_id]
+
+  if (!user_id)
+    # return empty if not logged in
+    return []
+  end
+
+  # get groups where user is a member of
+  groups = db.execute("SELECT 
+                        *
+                      FROM
+                        chat_groups
+                      INNER JOIN groups_users
+                      ON (chat_groups.id = groups_users.group_id)
+                      WHERE groups_users.user_id = ?
+                      ", user_id)
+
+  return groups
+end
+
+def username_exists(username)
+  db = connect_db()
+
+  # get the user id of user with username
+  user = db.execute("SELECT id FROM users WHERE username = ?", username).first
+
+  return user ? true : false
+end
+
+def user_exists_in_group(group_id, user_id)
+  db = connect_db()
+
+  is_member = db.execute("SELECT
+              1
+              FROM groups_users
+              WHERE user_id = ?
+              AND group_id = ?", user_id, group_id).first
+
+  return is_member ? true : false
+end
+
+def add_member_to_group(group_id, new_member_username)
+  db = connect_db()
+
+  # get the user id of user with username
+  if (!username_exists(new_member_username))
+    # display error message
+    raise "No user found with username #{new_member_username}"
+  end
+
+  user_id = user["id"]
+
+  if (user_exists_in_group(group_id, user_id))
+    # display error message
+    raise "User is already a member of the group"
+  end
+
+  puts "Adding user #{user_id} to group #{group_id}"
+  db.execute("INSERT INTO groups_users (user_id, group_id) VALUES (?, ?)", user_id, group_id)
+end
+
+def get_members_in_group(group_id)
+  if (!group_id)
+    return []
+  end
+
+  db = connect_db()
+
+  members = db.execute("SELECT
+                          users.id,
+                          users.username,
+                          users_group_roles.group_role_id
+                        FROM
+                          users
+                        INNER JOIN groups_users
+                          ON groups_users.user_id = users.id
+                        LEFT JOIN users_group_roles
+                          ON users_group_roles.user_id = users.id
+                          AND users_group_roles.group_role_id = (SELECT id FROM group_roles WHERE group_roles.group_id = ?)
+                        WHERE groups_users.group_id = ?
+                        ", group_id, group_id)
+
+  return members
+end
+
+def create_role(group_id, title, can_delete, can_kick)
+  db = connect_db()
+
+  db.execute("INSERT INTO group_roles (group_id, title, canDelete, canKick) VALUES (?, ?, ?, ?)", group_id, title, can_delete, can_kick)
+end
+
+def get_role(role_id)
+  if (!role_id)
+    return nil
+  end
+
+  db = connect_db()
+
+  role = db.execute("SELECT * FROM group_roles WHERE id = ?", role_id).first
+  return role
+end
+
+def get_roles_in_group(group_id)
+  db = connect_db()
+
+  group_roles = db.execute("SELECT * FROM group_roles WHERE group_id = ?", group_id)
+
+  return group_roles
+end
+
+def update_role(role_id, title, can_delete, can_kick)
+  db = connect_db()
+
+  db.execute("UPDATE group_roles
+              SET title = ?,
+                  canDelete = ?,
+                  canKick = ?
+              WHERE id = ?", title, can_delete, can_kick, role_id)
+end
+
+def update_role_of_user_in_group(group_id, user_id, role_id)
+  db = connect_db()
+
+  current_role_id = db.execute("SELECT 
+                                users_group_roles.group_role_id
+                              FROM 
+                                users_group_roles
+                              LEFT JOIN users
+                                ON users.id = users_group_roles.user_id
+                              INNER JOIN group_roles
+                                ON group_roles.id = users_group_roles.group_role_id
+                              WHERE group_roles.group_id = ?
+                              AND users.id = ?
+                              ", group_id, user_id).first
+
+  if (current_role_id)
+    if !role_id
+      # update role_id
+      db.execute("UPDATE users_group_roles
+                SET group_role_id = ?
+                WHERE users_group_roles.group_role_id =
+                  (SELECT id 
+                    FROM group_roles 
+                  WHERE group_id = ?)
+                AND user_id = ?", role_id, group_id, user_id)
+    else
+      # delete role from user
+      db.execute("DELETE FROM users_group_roles
+                  WHERE users_group_roles.group_role_id = (SELECT id FROM group_roles WHERE group_id = ?)
+                  AND user_id = ?", group_id, user_id)
+    end
+  else
+    # create new relation
+    db.execute("INSERT INTO users_group_roles (group_role_id, user_id) VALUES (?, ?)", role_id, user_id)
+  end
+end
+
+def delete_role_in_group(group_id, role_id)
+  db = connect_db()
+
+  db.execute("DELETE FROM group_roles, users_group_roles
+              FROM group_roles
+              INNER JOIN users_group_roles ON group_roles.id = users_group_roles.group_role_id
+              WHERE group_roles.id = ?
+              AND group_roles.group_id", role_id, group_id)
+end
